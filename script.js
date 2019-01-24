00c700c02bd2d5dd8c28d6ab13f19dd3df33f5b1
mahda kazemian <mahda628@yahoo.com>
2019-01-24 13:25:43 -0700

    adding table of images with description/considering id for index  and register pages.

--- script.js
+++ script.js
@@ -1,26 +1,74 @@
 
 
 // submit button
-var button = document.getElementById("submit");
-button.addEventListener("click", function (event) {
-event.preventDefault ();    
-var submit = confirm("Are you sure that you want to submit the form?");
-if (submit)
-console.log(submit);
-else 
-console.log(submit);
 
-});
+if (document.getElementById("bodyregister")){
+
+    var button = document.getElementById("submit");
+    button.addEventListener("click", function (event) {
+    event.preventDefault ();    
+    var submit = confirm("Are you sure that you want to submit the form?");
+    if (submit)
+    console.log(submit);
+    else 
+    console.log(submit);
+   
+    });
+
 // reset button
-var button = document.getElementById("reset");
-button.addEventListener("click", function (event) {
-event.preventDefault ();    
-var reset = confirm("Are you sure that you want to reset the form?");
-if (reset)
-console.log(reset);
-else 
-console.log(reset);
 
-});
+    var button = document.getElementById("reset");
+    button.addEventListener("click", function (event) {
+    event.preventDefault ();    
+    var reset = confirm("Are you sure that you want to reset the form?");
+    if (reset)
+    console.log(reset);
+    else 
+    console.log(reset);
+   
+    });
 
 
+    
+}
+ 
+
+
+if (document.getElementById("bodyindex")){
+
+    var immageArrey=["C://xampp/htdocs/mahda-project/images/pic8.jpg",
+                 "C://xampp/htdocs/mahda-project/images/pic9.jpg",
+                 "C://xampp/htdocs/mahda-project/images/pic10.jpg",
+                 "C://xampp/htdocs/mahda-project/images/pic11.jpg"];
+
+    var descriptionArrey=["Paris","Venice","Tehran","Moscow"];
+
+    var newTable=document.createElement("table");
+    for (i=0; i<immageArrey.length;i++ ){
+    var newTableRow=document.createElement("tr");
+
+    var newImg=document.createElement("img");
+    newImg.src=immageArrey[i];
+
+    var newTableData1=document.createElement("td");
+    newTableData1.appendChild(newImg);
+
+    newTableRow.appendChild(newTableData1);
+
+    var newCap=document.createElement("p");
+    var capText=document.createTextNode(descriptionArrey[i]);
+    newCap.appendChild(capText);
+
+    var newTableData2=document.createElement("td");
+    newTableData2.appendChild(newCap);
+
+    newTableRow.appendChild(newTableData2);
+    newTable.appendChild(newTableRow);
+
+    };
+    console.log(newTable);
+    var mainDetails=document.getElementsByClassName("maindetails")[0];
+    mainDetails.appendChild(newTable);
+
+
+}
