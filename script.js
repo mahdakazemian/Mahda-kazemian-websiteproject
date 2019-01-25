3c9b54fa2cc58e729c198cdf1cb327dd30a70971
mahda kazemian <mahda628@yahoo.com>
2019-01-25 00:31:30 -0700

    adding focus commands for form with their descriptions.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
+/////////register page////////
 
 // submit button
 
+/*
 if (document.getElementById("bodyregister")){
 
     var button = document.getElementById("submit");
@@ -27,19 +29,98 @@ if (document.getElementById("bodyregister")){
     console.log(reset);
    
     });
+}             
+*/
 
 
+/////////focus
+
+
+if (document.getElementById("bodyregister")){
+
+var fullName=document.getElementById("fullname");
+fullName.addEventListener("focus",function(){
+focusname.style.display = "block";
+});
+fullName.addEventListener('focusout', function() {
+focusname.style.display = 'none';
+});
     
-}
- 
 
+var userName=document.getElementById("username");
+userName.addEventListener("focus",function(){
+focususername.style.display = "block";
+});
+userName.addEventListener('focusout', function() {
+focususername.style.display = 'none';
+});
+    
+
+var passWord=document.getElementById("password");
+passWord.addEventListener("focus",function(){
+focuspassword.style.display = "block";
+});
+passWord.addEventListener('focusout', function() {
+focuspassword.style.display = 'none';
+});
+
+
+var phoneNumber=document.getElementById("phonenumber");
+phoneNumber.addEventListener("focus",function(){
+focusphone.style.display = "block";
+});
+phoneNumber.addEventListener('focusout', function() {
+focusphone.style.display = 'none';  
+});
+
+var Address=document.getElementById("address");
+Address.addEventListener("focus",function(){
+focusaddress.style.display = "block";
+});
+Address.addEventListener('focusout', function() {
+focusaddress.style.display = 'none';  
+});
+
+var City=document.getElementById("city");
+City.addEventListener("focus",function(){
+focuscity.style.display = "block";
+});
+City.addEventListener('focusout', function() {
+focuscity.style.display = 'none';  
+});
+
+var Province=document.getElementById("province");
+Province.addEventListener("focus",function(){
+focusprovince.style.display = "block";
+});
+Province.addEventListener('focusout', function() {
+focusprovince.style.display = 'none';  
+});
+
+
+var postaltest = RegExp(/^[A-Z]\d[A-Z] ?\d[A-Z]\d$/);
+var Postalcode = document.getElementById("postalcode");
+console.log (postaltest.test(postalcode));
+
+
+}
+
+
+
+
+
+
+
+//////////index page//////////
+
+///adding images and description inside the table
 
 if (document.getElementById("bodyindex")){
 
     var immageArrey=["C://xampp/htdocs/mahda-project/images/pic8.jpg",
-                 "C://xampp/htdocs/mahda-project/images/pic9.jpg",
-                 "C://xampp/htdocs/mahda-project/images/pic10.jpg",
-                 "C://xampp/htdocs/mahda-project/images/pic11.jpg"];
+                     "C://xampp/htdocs/mahda-project/images/pic9.jpg",
+                     "C://xampp/htdocs/mahda-project/images/pic10.jpg",
+                     "C://xampp/htdocs/mahda-project/images/pic11.jpg"];
 
     var descriptionArrey=["Paris","Venice","Tehran","Moscow"];
 
@@ -69,6 +150,9 @@ if (document.getElementById("bodyindex")){
     console.log(newTable);
     var mainDetails=document.getElementsByClassName("maindetails")[0];
     mainDetails.appendChild(newTable);
+
+    
+
 
 
 }
