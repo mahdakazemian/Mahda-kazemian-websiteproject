888357c9b5d2c59b19192b188095c88246f97d4e
mahda kazemian <mahda628@yahoo.com>
2019-01-26 07:31:11 -0700

    adding the error messages with java script and readme file.

---
+++ README.md
@@ -0,0 +1,18 @@
+Course name: Web Application Development
+
+Date:January 26 , 2019
+
+Hello
+My name is Mahda Kazemian. I'm the the OOSD program student at SAIT.
+This is my final assignment regarding design a web page through useing
+html, css,and java script. In this project there are lots of html commands 
+in order to how creat a web page with some paragraphs,pictures,and form,the 
+commands to style them in css ,and finaly various java script functions to do 
+specefic things in our website such as ( creating a table,click and focus functions,
+eventprevent,submit,reset, addlistener,settimeout,and etc). I belive it can be a good 
+example for begginer students who want to start to design a website. At the end, I would 
+like to thank to our kind,patient, and wonderful instructor Heather Tovey who guided me 
+during my project and I learned many things from her during my course.
+
+Course Module : CPRG-210-OSD.
+“Final Assignment”.
--- script.js
+++ script.js
@@ -1,39 +1,7 @@
 
 /////////register page////////
-
-// submit button
-
-/*
-if (document.getElementById("bodyregister")){
-
-    var button = document.getElementById("submit");
-    button.addEventListener("click", function (event) {
-    event.preventDefault ();    
-    var submit = confirm("Are you sure that you want to submit the form?");
-    if (submit)
-    console.log(submit);
-    else 
-    console.log(submit);
-   
-    });
-
-// reset button
-
-    var button = document.getElementById("reset");
-    button.addEventListener("click", function (event) {
-    event.preventDefault ();    
-    var reset = confirm("Are you sure that you want to reset the form?");
-    if (reset)
-    console.log(reset);
-    else 
-    console.log(reset);
-   
-    });
-}             
-*/
-
-
-/////////focus
+         
+///////////////////focus function////////////////////////
 
 
 if (document.getElementById("bodyregister")){
@@ -45,7 +13,7 @@ focusname.style.display = "block";
 fullName.addEventListener('focusout', function() {
 focusname.style.display = 'none';
 });
-    
+   
 
 var userName=document.getElementById("username");
 userName.addEventListener("focus",function(){
@@ -98,13 +66,146 @@ focusprovince.style.display = 'none';
 });
 
 
-var postaltest = RegExp(/^[A-Z]\d[A-Z] ?\d[A-Z]\d$/);
-var Postalcode = document.getElementById("postalcode");
-console.log (postaltest.test(postalcode));
+var Postal=document.getElementById("postalcode");
+Postal.addEventListener("focus",function(){
+focuscode.style.display = "block";
+});
+Postal.addEventListener('focusout', function() {
+focuscode.style.display = 'none';  
+});
 
 
-}
 
+/////////////////Error messages///////////////////////
+
+
+var submitButton = document.getElementById("submitbtn");
+var errorName = document.getElementById("errorname");
+var errorUser = document.getElementById("erroruser");
+var errorPass = document.getElementById("errorpass");
+var errorPhone = document.getElementById("errorphone");
+var errorAddress = document.getElementById("erroraddress");
+var errorCity = document.getElementById("errorcity");
+var errorPro = document.getElementById("errorpro");
+var errorPost = document.getElementById("errorpost");
+
+
+
+submitButton.addEventListener("click", function(event) {
+
+    errorName.style.display = "none";
+    errorUser.style.display = "none";
+    errorPass.style.display = "none";
+    errorPhone.style.display = "none";
+    errorAddress.style.display = "none";
+    errorCity.style.display = "none";
+    errorPro.style.display = "none";
+    errorpost.style.display= "none";
+    correctpcode.style.display = "none";
+
+    var name = document.form1.fullname.value;
+    var user = document.form1.username.value;
+    var pass = document.form1.password.value;
+    var phone = document.form1.phonenumber.value;
+    var adr = document.form1.address.value;
+    var city = document.form1.city.value;
+    var pro = document.form1.province.value;
+    var post = document.form1.postalcode.value;
+
+    if (!name){
+
+        event.preventDefault();
+        errorName.style.display="block";
+        return false;
+    }
+   
+    if (!user){
+
+        event.preventDefault();
+        errorUser.style.display="block";
+        return false;
+    }
+   
+    if (!pass){
+
+        event.preventDefault();
+        errorPass.style.display="block";
+        return false;
+    }
+   
+    if (!phone){
+
+        event.preventDefault();
+        errorPhone.style.display="block";
+        return false;
+    }
+   
+    if (!adr){
+
+        event.preventDefault();
+        errorAddress.style.display="block";
+        return false;
+    }
+   
+    if (!city){
+
+        event.preventDefault();
+        errorCity.style.display="block";
+        return false;
+    }
+   
+    if (!pro){
+
+        event.preventDefault();
+        errorPro.style.display="block";
+        return false;
+    }
+
+   if (!post){
+     event.preventDefault();
+     errorpost.style.display="block";
+     return false;
+   }
+    if (/^[A-Z]\d[A-Z] ?\d[A-Z]\d$/.test(post)) {
+         }else{
+        event.preventDefault();
+        correctpcode.style.display ="block";
+        return false;
+    }
+
+    
+        
+   
+
+    
+////////// submit confirmation
+    event.preventDefault ();    
+    var submitConfirm = confirm("Are you sure that you want to submit the form?");
+    if (submitConfirm){
+        document.form1.submit();
+    }
+    
+   
+  });////end of click function for validation & submit
+
+/////////// reset confirmation
+
+    var resetbutton = document.getElementById("resetbtn");
+    resetbutton.addEventListener("click", function (event) {
+    event.preventDefault ();    
+    var resetconfirm = confirm("Are you sure that you want to reset the form?");
+    if (resetconfirm){
+        document.form1.reset();
+    
+    }
+   
+    });
+
+
+
+
+
+}///////// end of bodyregister
 
 
 
@@ -117,6 +218,12 @@ console.log (postaltest.test(postalcode));
 
 if (document.getElementById("bodyindex")){
 
+    var urlArrey=[ "https://en.parisinfo.com/","https://www.lonelyplanet.com/italy/venice",
+                     "https://wikitravel.org/en/Tehran","https://wikitravel.org/en/Moscow"]
+
+
+
+
     var immageArrey=["C://xampp/htdocs/mahda-project/images/pic8.jpg",
                      "C://xampp/htdocs/mahda-project/images/pic9.jpg",
                      "C://xampp/htdocs/mahda-project/images/pic10.jpg",
@@ -125,15 +232,15 @@ if (document.getElementById("bodyindex")){
     var descriptionArrey=["Paris","Venice","Tehran","Moscow"];
 
     var newTable=document.createElement("table");
-    for (i=0; i<immageArrey.length;i++ ){
-    var newTableRow=document.createElement("tr");
 
+    for (i=0; i<immageArrey.length;i++ ){
+
+    var newTableRow=document.createElement("tr");
     var newImg=document.createElement("img");
     newImg.src=immageArrey[i];
 
     var newTableData1=document.createElement("td");
     newTableData1.appendChild(newImg);
-
     newTableRow.appendChild(newTableData1);
 
     var newCap=document.createElement("p");
@@ -153,6 +260,13 @@ if (document.getElementById("bodyindex")){
 
     
 
+/*
+   var my_window=window.open(urlArrey[i]);
+   newImg.appendChild(my_window);
+   my_window.close();
+
+
+*/
 
 
 }
